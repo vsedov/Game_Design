b5b3c794bd1317fa999379b11ffeecd6355b9f8b
vsedov <viv.sedov@hotmail.com>
2021-02-12 18:55:41 +0000

    Setup CFG Addded

--- src/main.py
+++ src/main.py
@@ -2,14 +2,24 @@
 # -- coding: utf-8 --
 # vim:fenc=utf-8
 
-
-import SimpleGUICS2Pygame.simpleguics2pygame as simplegui  # pyflakes.ignore
+import SimpleGUICS2Pygame.simpleguics2pygame as simplegui
 from frosch import hook
+
+from system_components.control import Control
+from system_components.frame import frame_height, frame_width
+from system_components.Vector import Vector
+
+
+class SnakeMain(Control):
+    def _init__(self, var=Vector(0, 0)):
+        pass
 
 
 def main() -> None:
+    frame = simplegui.create_frame("Snake Game", frame_width, frame_height)
+    frame.set_draw_handler(...)
 
-    ...
+    frame.start()
 
 
 if __name__ == "__main__":
