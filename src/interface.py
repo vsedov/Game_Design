bb3e601b3fe7d7b0f2ff140ef40661527e639d9a
vsedov <viv.sedov@hotmail.com>
2021-02-15 15:01:08 +0000

    KeyMaping Added

---
+++ src/interface.py
@@ -0,0 +1,35 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+
+import SimpleGUICS2Pygame.simpleguics2pygame as simplegui  # pyflakes.ignore
+
+from main import DataContainer  # flake8: noqa
+from main import SnakeMain
+
+
+class Interface(SnakeMain):
+    def __init__(self):
+        super(Interface, self).__init__()
+
+    def key_down(self, key_item):
+        if key_item == simplegui.KEY_MAP[x:=("right")] and self.velocity.x == 0:
+            self.position_direction(x)
+
+        elif key_item == simplegui.KEY_MAP[x:=("left")] and self.velocity.x == 0:
+            self.position_direction(x)
+
+        elif key_item == simplegui.KEY_MAP[x:=("up")] and self.velocity.y == 0 :
+            self.position_direction("up")
+
+        elif key_item == simplegui.KEY_MAP[x:=("down")] and self.velocity == 0:
+            self.position_direction("down")
+        
+
+def main() -> None:
+    pointer = Interface()
+    pointer.checker()
+
+
+if __name__ == "__main__":
+    main()
