d7a06e891320ddfb06749ef6c9752021fd7a1084
vsedov <viv.sedov@hotmail.com>
2021-03-01 21:06:22 +0000

    Basic interface added , just in case the other one does not work

--- src/interface.py
+++ src/interface.py
@@ -1,34 +1,47 @@
 #! /usr/bin/env python3
 # -*- coding: utf-8 -*-
 # vim:fenc=utf-8
+#
+# File Name: interface
+
+
+import argparse
 
 import SimpleGUICS2Pygame.simpleguics2pygame as simplegui  # pyflakes.ignore
 
-from main import DataContainer  # flake8: noqa
-from main import SnakeMain
+from main import Colors, Game_Control
+from system_components.frame import frame_height, frame_width
 
 
-class Interface(SnakeMain):
-    def __init__(self):
-        super().__init__()
+def push_to_class(**kwargs):
+    legnth = kwargs.get("legnth")
+    # Here you can add any other thing you want for now , if you want to use cli -< Very much expandable
+    print(legnth)
 
-    def key_down(self, key_item):
-        if key_item == simplegui.KEY_MAP[(x := ("right"))] and self.velocity.x == 0:
-            self.position_direction(x)
+    snake = Game_Control(legnth)
 
-        elif key_item == simplegui.KEY_MAP[(x := ("left"))] and self.velocity.x == 0:
-            self.position_direction(x)
+    frame = simplegui.create_frame("Snake", frame_width, frame_height)
+    frame.set_keydown_handler(snake.key_down)
+    frame.set_draw_handler(snake.draw_self)
+    frame.set_canvas_background(Colors.BACKGROUND_COLOR)
 
-        elif key_item == simplegui.KEY_MAP[(x := ("up"))] and self.velocity.y == 0:
-            self.position_direction("up")
-
-        elif key_item == simplegui.KEY_MAP[(x := ("down"))] and self.velocity == 0:
-            self.position_direction("down")
+    timer = simplegui.create_timer(100, snake.timer_handler)
+    timer.start()
+    frame.start()
 
 
 def main() -> None:
-    pointer = Interface()
-    pointer.checker()
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "--x",
+        metavar="legnth",
+        type=int,
+        default=1.0,
+        help="Enter number for snake legnth?",
+    )
+    args = parser.parse_args()
+    push_to_class(legnth=args.x)
 
 
 if __name__ == "__main__":
