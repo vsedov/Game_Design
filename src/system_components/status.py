7c2de21c10d45d6f20c9f609ad1959a3f29aa374
vsedov <viv.sedov@hotmail.com>
2021-03-01 21:07:45 +0000

    remove dummy file

---
+++ src/system_components/status.py
@@ -0,0 +1,15 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+
+from dataclasses import dataclass
+
+
+@dataclass
+class GameState:
+    GAME_STATE: bool = False
+    RUNNING: bool = False
+    FINISHED: bool = False
+    POINTS: int = 0
+    SPEED: int = 100
+    internal_grid = 20
