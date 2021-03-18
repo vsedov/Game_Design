71d6f191bcd6646c17c49be910172c8adac92818
ZJAC281 <75216562+ZJAC281@users.noreply.github.com>
2021-03-18 19:47:39 +0000

    Update menu.py

--- src/menu.py
+++ src/menu.py
@@ -1,51 +1,73 @@
 try:
     import simplegui
-except ImportError:
+except ImportError :
     import SimpleGUICS2Pygame.simpleguics2pygame as simplegui
 import sys
+from Game_Start import GameStart
+##import codeskulptor
+from time import sleep
 
-IMG = simplegui.load_image("http://personal.rhul.ac.uk/zjac/281/snake.png")
-# IMG_CENTRE = (200, 200)
-# IMG_DIMS = (400, 400)
+IMG = simplegui.load_image('http://personal.rhul.ac.uk/zjac/281/snake.png')
+#IMG_CENTRE = (200, 200)
+#IMG_DIMS = (400, 400)
 IMG_CENTRE = (78, 66)
 IMG_DIMS = (156, 132)
+sound = simplegui.load_sound("http://commondatastorage.googleapis.com/codeskulptor-assets/week7-brrring.m4a")
+    
+class Menu:
+#    def __init__(self):
+        
+
+    def click(pos):
+        if (pos[0] >= 128 and pos[0] <= 384):
+            if (pos[1] >= 0 and pos[1] <= 64):
+                print("EASY")
+                sound.play()
+                GameStart(120)
+                
+            elif (pos[1] >= 128 and pos[1] <= 192):
+                print("Medium")
+                sound.play()
+                GameStart(140)
+                
+
+            elif (pos[1] >= 256 and pos[1] <= 320):
+                print("Hard")
+                sound.play()
+                GameStart(160)
+            
+            elif (pos[1] >= 384 and pos[1] <= 448):
+                sound.play()
+                print("Exit")
+                sleep(1.00)
+                sys.exit("user option was to leave " )
+                
 
 
-def click(pos):
-    if pos[0] >= 100 and pos[0] <= 300:
-        if pos[1] >= 50 and pos[1] <= 100:
-            print("Start")
+    def draw(canvas):
+        canvas.draw_image(IMG, IMG_CENTRE, IMG_DIMS, (256, (2*512 /3)), (512,512))
 
-        elif pos[1] >= 150 and pos[1] <= 200:
-            print("Options")
+        canvas.draw_polygon([(128, 64), (384, 64), (384, 0), (128, 0)], 5, '#660099')
+        canvas.draw_text('Easy', (204.8, 45), 23, 'White', 'monospace')
+        
+        canvas.draw_polygon([(128, 192), (384, 192), (384, 128), (128, 128)], 5, '#660099')
+        canvas.draw_text('Medium', (204.8, 170), 23, 'White', 'monospace')
+        
+        canvas.draw_polygon([(128, 320), (384, 320), (384, 256), (128, 256)], 5, '#660099')
+        canvas.draw_text('Hard', (217.6, 295), 23, 'White','monospace')
+        
+        canvas.draw_polygon([(128, 448), (384, 448), (384, 384), (128, 384)], 5, '#660099')
+        canvas.draw_text('Exit', (230.4, 420), 23, 'White','monospace')
+        
+        canvas.draw_text('High score: ', (339.2, 486.4), 23, 'White','monospace')
 
-        elif pos[1] >= 250 and pos[1] <= 300:
-            print("Exit")
-            sys.exit("user option was to leave ")
-
-
-def draw(canvas):
-    canvas.draw_image(IMG, IMG_CENTRE, IMG_DIMS, (200, (2 * 300 / 3)), (400, 400))
-
-    canvas.draw_polygon([(100, 50), (200, 50), (200, 100), (100, 100)], 5, "Blue")
-    canvas.draw_text("1 player", (130, 75), 12, "Red")
-
-    canvas.draw_polygon([(200, 50), (300, 50), (300, 100), (200, 100)], 5, "Blue")
-    canvas.draw_text("2 player", (230, 75), 12, "Red")
-
-    canvas.draw_polygon([(100, 150), (300, 150), (300, 200), (100, 200)], 5, "Blue")
-    canvas.draw_text("Options", (200, 175), 12, "Red")
-
-    canvas.draw_polygon([(100, 250), (300, 250), (300, 300), (100, 300)], 5, "Blue")
-    canvas.draw_text("Exit", (200, 275), 12, "Red")
-
-    canvas.draw_text("High score: ", (35, 380), 12, "Red")
+        
 
 
-frame = simplegui.create_frame("Home", 512, 512)
-frame.set_canvas_background("#2C6A6A")
-frame.set_mouseclick_handler(click)
+    frame = simplegui.create_frame("Home", 512, 512)
+    frame.set_canvas_background('#2C6A6A')
+    frame.set_mouseclick_handler(click)
 
-frame.set_draw_handler(draw)
+    frame.set_draw_handler(draw)
 
-frame.start()
+    frame.start()
