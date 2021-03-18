d5ab178e924e4e3338c63c04047dafe0ac117498
Ahmed Elsakaan <ahmedtarek3214@gmail.com>
2021-03-18 17:56:43 +0000

    feat: adds colour options

--- src/Game_Start.py
+++ src/Game_Start.py
@@ -15,6 +15,8 @@ __author__ = "Viv Sedov"
 def game_restart():
     ...
 
+def colour_button_handler(snake, colour):
+    snake.color.SNAKE_COLOR = colour
 
 class GameStart:
     # Can be omitted, Python will give a default implementation
@@ -24,11 +26,38 @@ class GameStart:
         snake = Game_Control(kwargs.get("length", 5))
 
         snake.color.SNAKE_COLOR = kwargs.get("colours", "purple")
+        def red_button_handler():
+            snake.color.SNAKE_COLOR = "Red"
+        def orange_button_handler():
+            snake.color.SNAKE_COLOR = "Orange"
+        def yellow_button_handler():
+            snake.color.SNAKE_COLOR = "Yellow"
+        def green_button_handler():
+            snake.color.SNAKE_COLOR = "Green"
+        def blue_button_handler():
+            snake.color.SNAKE_COLOR = "Blue"
+        def purple_button_handler():
+            snake.color.SNAKE_COLOR = "Purple"
+        def pink_button_handler():
+            snake.color.SNAKE_COLOR = "Pink"
 
         frame = simplegui.create_frame("Snake", frame_width, frame_height)
         frame.set_keydown_handler(snake.key_down)
         frame.set_draw_handler(snake.draw_self)
-
+        frame.add_label("Colour Options")
+        frame.add_button("Red", red_button_handler)
+        frame.add_label("")
+        frame.add_button("Orange", orange_button_handler)
+        frame.add_label("")
+        frame.add_button("Yellow", yellow_button_handler)
+        frame.add_label("")
+        frame.add_button("Green", green_button_handler)
+        frame.add_label("")
+        frame.add_button("Blue", blue_button_handler)
+        frame.add_label("")
+        frame.add_button("Purple", purple_button_handler)
+        frame.add_label("")
+        frame.add_button("Pink", pink_button_handler)
         # We do not change the background colour
         frame.set_canvas_background(snake.color.BACKGROUND_COLOR)
 
@@ -50,4 +79,4 @@ Steps :
  finished , uncomment this again . 
 """
 
-# GameStart()
+GameStart()
