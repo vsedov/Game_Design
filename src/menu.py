421ed01a7af1b8f9a17e44e3f16a2c333c692984
vsedov <viv.sedov@hotmail.com>
2021-03-18 22:59:25 +0000

    Commented out removed uneeded stuff

--- src/menu.py
+++ src/menu.py
@@ -1,54 +1,54 @@
+import sys
 from time import sleep  # import codeskulptor
 
 import SimpleGUICS2Pygame.simpleguics2pygame as simplegui
-
-IMG = simplegui.load_image("http://personal.rhul.ac.uk/zjac/281/snake.png")
-# IMG_CENTRE = (200, 200)
-# IMG_DIMS = (400, 400)
-IMG_CENTRE = (78, 66)
-IMG_DIMS = (156, 132)
-sound = simplegui.load_sound(
-    "http://commondatastorage.googleapis.com/codeskulptor-assets/week7-brrring.m4a"
-)
+from icecream import ic
 
 
 class Menu:
     def __init__(self):
         self.option: bool = False
+        self.IMG = simplegui.load_image("http://personal.rhul.ac.uk/zjac/281/snake.png")
+        self.IMG_CENTRE = (78, 66)
+        self.IMG_DIMS = (156, 132)
+        self.sound = simplegui.load_sound(
+            "http://commondatastorage.googleapis.com/codeskulptor-assets/week7-brrring.m4a"
+        )
 
+    # This is active before eveyrthing else woudl starrt
     def __start_game(self, main_length: int, speed: int):
-
-        if self.option:
-            # GameStart(length=main_length)
-            pass
+        if self.option is True:
+            # pointer = GameStart(length=main_length)
+            print(pointer)
 
     def click(self, pos):
         if pos[0] >= 128 and pos[0] <= 384:
             if pos[1] >= 0 and pos[1] <= 64:
-                self.option = True
-                print("EASY")
-                sound.play()
+                ic("Easy")
+                self.sound.play()
                 # GameStart(120)
-                self.__start_game(main_length=5, speed=100)
+                # self.__start_game(main_length=5, speed=100)
 
             elif pos[1] >= 128 and pos[1] <= 192:
                 self.option = True
-                print("Medium")
-                sound.play()
-                self.__start_game(main_length=15, speed=80)
+                ic("Medium")
+                self.sound.play()
+                # self.__start_game(main_length=15, speed=80)
 
             elif pos[1] >= 256 and pos[1] <= 320:
-                self.option = True
-                sound.play()
-                self.__start_game(main_length=25, speed=50)
+                ic("Hard")
+                self.sound.play()
+                # self.__start_game(main_length=25, speed=50)
 
             elif pos[1] >= 384 and pos[1] <= 448:
-                sound.play()
+                self.sound.play()
                 sleep(1.00)
-                exit()
+                sys.exit()
 
     def draw(self, canvas):
-        canvas.draw_image(IMG, IMG_CENTRE, IMG_DIMS, (256, (2 * 512 / 3)), (512, 512))
+        canvas.draw_image(
+            self.IMG, self.IMG_CENTRE, self.IMG_DIMS, (256, (2 * 512 / 3)), (512, 512)
+        )
 
         canvas.draw_polygon([(128, 64), (384, 64), (384, 0), (128, 0)], 5, "#660099")
         canvas.draw_text("Easy", (204.8, 45), 23, "White", "monospace")
