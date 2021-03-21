6d6fb7e3388f6626312ccb0f4f27fa5ae6c3ecff
Max Fisher <75727247+ItsM9@users.noreply.github.com>
2021-03-21 14:06:27 +0000

    Update menu.py

--- src/menu.py
+++ src/menu.py
@@ -26,14 +26,14 @@ class Menu:
         """
         self.option: bool = False
         self.IMG = simplegui.load_image("http://personal.rhul.ac.uk/zjac/281/snake.png")
-	self.l1 = [-12,-8,-4,0,4,8,12]
-	self.IMG2 = simplegui.load_image('http://personal.rhul.ac.uk/zjac/281/snake.png')
-	self.l2 = [-11,-7,-3,1,5,9,13]
-	self.IMG3 = simplegui.load_image('https://opengameart.org/sites/default/files/styles/medium/public/apple_1_0.png')
-	self.l3 = [-10,-6,-2,2,6,10,14]
-	self.IMG4 = simplegui.load_image('https://opengameart.org/sites/default/files/styles/medium/public/SneckoCreature.PNG')
-	self.l4 = [-9,-5,-1,3,7,11,15]
-	self.theme = 1
+	    self.l1 = [-12,-8,-4,0,4,8,12]
+	    self.IMG2 = simplegui.load_image('http://personal.rhul.ac.uk/zjac/281/snake.png')
+	    self.l2 = [-11,-7,-3,1,5,9,13]
+	    self.IMG3 = simplegui.load_image('https://opengameart.org/sites/default/files/styles/medium/public/apple_1_0.png')
+	    self.l3 = [-10,-6,-2,2,6,10,14]
+	    self.IMG4 = simplegui.load_image('https://opengameart.org/sites/default/files/styles/medium/public/SneckoCreature.PNG')
+	    self.l4 = [-9,-5,-1,3,7,11,15]
+	    self.theme = 1
         self.IMG_CENTRE = (78, 66)
         self.IMG_DIMS = (156, 132)
         self.sound = simplegui.load_sound(
@@ -86,7 +86,7 @@ class Menu:
                 sleep(1.00)
                 sys.exit()
 	        
-	global theme
+	    global theme
         if (pos[1] >=30 and pos [1] <= 48):
             if (pos[0] >=460  and pos[0] <=475):
                 theme -= 1
@@ -114,7 +114,7 @@ class Menu:
         elif self.theme in self.l4:
             canvas.draw_image(self.IMG4, self.IMG_CENTRE, self.IMG_DIMS, (256, (2*512 /4)), (512,512))
 
-	canvas.draw_polygon([(460, 48), (506, 48), (506, 0), (460, 0)], 5, '#660099')
+	    canvas.draw_polygon([(460, 48), (506, 48), (506, 0), (460, 0)], 5, '#660099')
         canvas.draw_text('Theme', (463, 27), 15, 'White', 'monospace')
         canvas.draw_text('<--', (465, 40), 15, 'White', 'monospace')
         canvas.draw_text('-->', (478, 40), 15, 'White', 'monospace')
