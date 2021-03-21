e3232341c79231980d1a965cceb07ae61cb3cd2f
vsedov <viv.sedov@hotmail.com>
2021-03-21 15:42:49 +0000

    Testing multiple players started algo

--- src/control.py
+++ src/control.py
@@ -76,7 +76,7 @@ class Game_Control(Snake_Main):
 
             "You have to stop the timer : before writing otherwise you get multiple write instances"
             self.timer.stop()
-            JsonData(10000, "test")
+            JsonData(50, "anothertest")
             self.frame.stop()
 
     def speed_increase(self):
