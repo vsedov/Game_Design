29e044efa5f87268e88376794d5282db07b4c08e
vsedov <viv.sedov@hotmail.com>
2021-03-25 00:15:35 +0000

    Removed personal infomation

--- Runner.py
+++ Runner.py
@@ -2,13 +2,7 @@
 # -*- coding: utf-8 -*-
 # vim:fenc=utf-8
 #
-# Copyright © 2021-03-24 18:17 Viv Sedov
-#
 # File Name: Runner
-__author__ = "Viv Sedov"
-__email__ = "viv.sv@hotmail.com"
-
-from frosch import hook
 
 from src.menu_controller import itself
 
@@ -19,5 +13,4 @@ def main() -> None:
 
 
 if __name__ == "__main__":
-    hook()
     main()
